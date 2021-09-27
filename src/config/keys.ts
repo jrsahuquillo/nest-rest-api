26ce210ce6aadd562ad7fdb964456ff70ab43510
José Ramón Sahuquillo Caballero <jrsahuquillo@gmail.com>
2021-09-27 17:38:23 +0200

    Update keys.ts

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,3 +1,3 @@
 export default {
-  mongoURI: 'mongodb+srv://sahu:sahu@cluster0.lsb12.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://<user>:<password>@cluster0.lsb12.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
